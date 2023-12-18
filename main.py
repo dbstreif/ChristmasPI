b1ccd76041858be027a4b4c5bc2da1072f93dca6
Dom13377 <geegne123@gmail.com>
2023-12-18 13:36:56 -0800

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+from spidev import SpiDev
+
+spi = SpiDev()
+spi.open(1, 1)
+spi.max_speed_hz = 4000
+msg = [0b00000000000000000000000000000000, 0b11100000000000000000000000000, 0b11111111111111111111111111111111]
+
+spi.xfer(msg)
+
+spi.close()
